6a3d9fa2c7bbdd45eb67be23e245b0a147d49964
nayran <nayranziebell@icloud.com>
2021-12-16 15:30:51 -0300

    ft_server

--- 05 - ft_server/srcs/utils.sh
+++
@@ -1,7 +0,0 @@
-## utilitarios
-apt-get update
-apt-get install -y --no-install-recommends apt-utils
-apt-get -y install wget
-
-## instalacao e configuracao de servicos
-bash /root/configurations.sh
